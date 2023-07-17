11501fd025c0461f0e1805a764ce6ed7548cd64a
gihanwijesinghe <gihanwijesinghe12@gmail.com>
2023-07-17 16:55:53 +0530

    micro front end using model federation

--- src/App.js
+++ src/App.js
@@ -1,23 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+// import "./App.css";
+// import React from "react";
+
+// function App() {
+//   return (
+//     <div className="App">
+//       <div className="container">This is my home page</div>
+//     </div>
+//   );
+// }
+
+// export default App;
+
+import React, { lazy, Suspense } from "react"; // Must be imported for webpack to work
+import "./App.css";
+
+const Header = lazy(() => import("HeaderApp/Header"));
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Suspense fallback={<div>Loading Header...</div>}>
+        <Header />
+      </Suspense>
+      <div className="container">Demo home page testing</div>
     </div>
   );
 }
